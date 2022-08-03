1ca180919aaca08a274d6417a63500fa8a4c4e90
Mehmet Duran <mehmet.duran@gmail.com>
2022-08-03 03:00:27 +0300

    Finished for now

--- src/js/index.js
+++ src/js/index.js
@@ -2,18 +2,42 @@ import '../scss/main.scss';
 
 document.addEventListener('DOMContentLoaded', setup);
 
-function setup(){
-    document.querySelector('#water-amount').addEventListener('input', calculate);
-    document.querySelector('#coffee-per-litre').addEventListener('input', calculate);
+const ff = new Intl.NumberFormat('en-GB', {
+  style: 'decimal',
+  maximumFractionDigits: 2
+}).format;
+
+function setup() {
+  document.querySelector('#water-amount')?.addEventListener('input', calculate);
+  document.querySelector('#coffee-per-litre')?.addEventListener('input', calculate);
+  document.querySelector('#ground-weight')?.addEventListener('input', calculate);
 }
 
-function calculate(){
-    const waterAmount = document.querySelector('#water-amount').value;
-    const coffeePerLitre = document.querySelector('#coffee-per-litre').value;
-    const groundWeight = coffeePerLitre * waterAmount / 1000;
+function calculate( event ) {
+  const coffeePerLitreInput = document.querySelector('#coffee-per-litre')?.value;
+  const waterAmountInput = document.querySelector('#water-amount')?.value;
+  const groundWeightInput = document.querySelector('#ground-weight')?.value;
 
-    document.querySelector('#ground-weight').value = groundWeight;
-    document.querySelector('#bloom-range').value = `${groundWeight * 2}gr to ${groundWeight * 3}gr`;
-    document.querySelector('#sixty-percent').value = waterAmount * .6;
-    document.querySelector('#forty-percent').value = waterAmount * .4;
+  let coffeePerLitre = coffeePerLitreInput;
+  let waterAmount = waterAmountInput;
+  let groundWeight = groundWeightInput;
+
+  if( event.target?.id === 'water-amount' ) {
+    groundWeight = waterAmount * coffeePerLitre / 1000;
+  }else if ( event.target?.id === 'coffee-per-litre' ){
+    groundWeight = waterAmount * coffeePerLitre / 1000;
+  }else if ( event.target?.id === 'ground-weight' ){
+    waterAmount = groundWeight * 1000 / coffeePerLitre;
+  }else
+    return false;
+
+  document.querySelector('#water-amount').value = waterAmount;
+  document.querySelector('#coffee-per-litre').value = coffeePerLitre;
+  document.querySelector('#ground-weight').value = groundWeight;
+  document.querySelector('#bloom-range').innerHTML = `${ff(groundWeight * 2)}gr to ${ff(groundWeight * 3)}gr`;
+  document.querySelector('#sixty-percent').innerHTML = `${ff(waterAmount * .6)}gr`;
+  document.querySelector('#forty-percent').innerHTML = `${ff(waterAmount * .4)}gr`;
+  document.querySelector('#ice-weight').innerHTML = `${ff(waterAmount * .4)}gr`;
+  document.querySelector('#ice-water-weight').innerHTML = `${ff(waterAmount * .6)}gr`;
+  document.querySelector('#iced-grounds-weight').innerHTML = `${ff(groundWeight * (1 + (1/6)))}gr`;
 }
--- static/js/main.js
+++ static/js/main.js
@@ -3015,20 +3015,42 @@ module.exports = function (updatedModules, renewedModules) {
 __webpack_require__(23);
 
 document.addEventListener('DOMContentLoaded', setup);
+const ff = new Intl.NumberFormat('en-GB', {
+  style: 'decimal',
+  maximumFractionDigits: 2
+}).format;
 
 function setup() {
-  document.querySelector('#water-amount').addEventListener('input', calculate);
-  document.querySelector('#coffee-per-litre').addEventListener('input', calculate);
+  document.querySelector('#water-amount')?.addEventListener('input', calculate);
+  document.querySelector('#coffee-per-litre')?.addEventListener('input', calculate);
+  document.querySelector('#ground-weight')?.addEventListener('input', calculate);
 }
 
-function calculate() {
-  const waterAmount = document.querySelector('#water-amount').value;
-  const coffeePerLitre = document.querySelector('#coffee-per-litre').value;
-  const groundWeight = coffeePerLitre * waterAmount / 1000;
+function calculate(event) {
+  const coffeePerLitreInput = document.querySelector('#coffee-per-litre')?.value;
+  const waterAmountInput = document.querySelector('#water-amount')?.value;
+  const groundWeightInput = document.querySelector('#ground-weight')?.value;
+  let coffeePerLitre = coffeePerLitreInput;
+  let waterAmount = waterAmountInput;
+  let groundWeight = groundWeightInput;
+
+  if (event.target?.id === 'water-amount') {
+    groundWeight = waterAmount * coffeePerLitre / 1000;
+  } else if (event.target?.id === 'coffee-per-litre') {
+    groundWeight = waterAmount * coffeePerLitre / 1000;
+  } else if (event.target?.id === 'ground-weight') {
+    waterAmount = groundWeight * 1000 / coffeePerLitre;
+  } else return false;
+
+  document.querySelector('#water-amount').value = waterAmount;
+  document.querySelector('#coffee-per-litre').value = coffeePerLitre;
   document.querySelector('#ground-weight').value = groundWeight;
-  document.querySelector('#bloom-range').value = `${groundWeight * 2}gr to ${groundWeight * 3}gr`;
-  document.querySelector('#sixty-percent').value = waterAmount * .6;
-  document.querySelector('#forty-percent').value = waterAmount * .4;
+  document.querySelector('#bloom-range').innerHTML = `${ff(groundWeight * 2)}gr to ${ff(groundWeight * 3)}gr`;
+  document.querySelector('#sixty-percent').innerHTML = `${ff(waterAmount * .6)}gr`;
+  document.querySelector('#forty-percent').innerHTML = `${ff(waterAmount * .4)}gr`;
+  document.querySelector('#ice-weight').innerHTML = `${ff(waterAmount * .4)}gr`;
+  document.querySelector('#ice-water-weight').innerHTML = `${ff(waterAmount * .6)}gr`;
+  document.querySelector('#iced-grounds-weight').innerHTML = `${ff(groundWeight * (1 + 1 / 6))}gr`;
 }
 
 /***/ }),
@@ -3040,7 +3062,7 @@ __webpack_require__.r(__webpack_exports__);
 // extracted by mini-css-extract-plugin
 
     if(true) {
-      // 1659463370061
+      // 1659484775627
       var cssReload = __webpack_require__(24)(module.id, {"locals":false});
       module.hot.dispose(cssReload);
       module.hot.accept(undefined, cssReload);
@@ -3474,7 +3496,7 @@ module.exports = function (urlString) {
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("1e7ea66bd1881ca6e080")
+/******/ 		__webpack_require__.h = () => ("60b9676b2445eecdf438")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
